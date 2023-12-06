416219382e7ca54a64ec47e34ce368fe1d5fa55c
Nicholas Shedd <nshedd99@gmail.com>
2023-12-05 21:32:48 -0800

    Functional Database

--- dreamjournal/server/db.js
+++ dreamjournal/server/db.js
@@ -6,10 +6,15 @@ const db = new sqlite3.Database('dreams.db');
 
 // Create dreams table if not exists
 db.run(`
-  CREATE TABLE IF NOT EXISTS dreams (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT
-  )
+CREATE TABLE IF NOT EXISTS dreams (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   title TEXT NOT NULL,
+   description TEXT,
+   date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);
+   
+INSERT INTO dreams (title, description, date) VALUES
+   ('First Dream', 'This is my first dream journal entry.', '2023-12-10'),
+   ('Second Dream', 'I had a strange dream last night.', '2023-12-11');
 `);
 
 export { db }; // Export the database instance directly
--- dreamjournal/server/server.js
+++ dreamjournal/server/server.js
@@ -1,7 +1,7 @@
 // server.js
 import express from 'express';
 import bodyParser from 'body-parser';
-import { db } from './db.js'; // Import the database instance directly
+import { db } from './db.js';
 
 const app = express();
 const port = 3000;
@@ -10,8 +10,8 @@ app.use(bodyParser.json());
 
 // API endpoint to save a dream
 app.post('/api/dreams', async (req, res) => {
-  const { content } = req.body;
-  db.run('INSERT INTO dreams (content) VALUES (?)', [content], (err) => {
+  const { title, description, date } = req.body;
+  db.run('INSERT INTO dreams (title, description, date) VALUES (?, ?, ?)', [title, description, date], function (err) {
     if (err) {
       console.error(err.message);
       res.status(500).send('Internal Server Error');
@@ -29,6 +29,35 @@ app.get('/api/dreams', async (req, res) => {
       res.status(500).send('Internal Server Error');
     } else {
       res.json(rows);
+    }
+  });
+});
+
+// API endpoint to update a dream
+app.put('/api/dreams/:id', async (req, res) => {
+  const dreamId = req.params.id;
+  const { title, description, date } = req.body;
+
+  db.run('UPDATE dreams SET title = ?, description = ?, date = ? WHERE id = ?', [title, description, date, dreamId], function (err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.status(200).send('Dream updated successfully');
+    }
+  });
+});
+
+// API endpoint to delete a dream
+app.delete('/api/dreams/:id', async (req, res) => {
+  const dreamId = req.params.id;
+
+  db.run('DELETE FROM dreams WHERE id = ?', [dreamId], function (err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.status(200).send('Dream deleted successfully');
     }
   });
 });
